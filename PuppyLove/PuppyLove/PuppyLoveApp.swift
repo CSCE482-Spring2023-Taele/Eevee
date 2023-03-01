905609ba3eaa385989bda164b259484cc255feb1
user <user@users-Mac.local>
2023-02-28 19:34:48 -0600

    skeleton

---
+++ PuppyLove/PuppyLove/PuppyLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyLoveApp.swift
+//  PuppyLove
+//
+//  Created by user on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PuppyLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
