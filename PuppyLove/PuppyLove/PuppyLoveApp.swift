a265d474ac428ad04cbd5b0d932a5ce1708791c3
Aaron Sanchez <sanchez@Bellas-MBP.fios-router.home>
2023-03-15 00:29:53 -0500

    Removed weird subfolder

---
+++ PuppyLove/PuppyLove/PuppyLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyLoveApp.swift
+//  PuppyLove
+//
+//  Created by Bella Sanchez on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PuppyLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
