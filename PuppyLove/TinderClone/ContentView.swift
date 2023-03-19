7f686e9aadc3f371ba3a90ca068f3c68bc5253f9
Jennifer Choudhury <jenniferchoudhury@Jennifers-MacBook-Pro.local>
2023-03-19 16:58:27 -0500

    swipe

---
+++ PuppyLove/TinderClone/ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            NavigationSection()
+            CardsSection()
+            FooterSection()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
+
+
+
---
+++ PuppyLove/TinderClone/PuppyLoveApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PuppyLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
