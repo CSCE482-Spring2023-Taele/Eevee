a0f684ff2b3f35d22788aaf16a316c5202a5b466
aaronsanchez01 <aaronsanchez01@tamu.edu>
2023-04-20 00:08:23 -0500

    Added matching algorithm card sorting.

--- PuppyLoveAPI/Controllers/SwipeList.cs
+++ PuppyLoveAPI/Controllers/SwipeList.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Mysqlx.Crud;
+using System.Net;
 using System.Net.Mime;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Json;
 
 namespace PuppyLoveAPI.Controllers
@@ -11,21 +13,26 @@ namespace PuppyLoveAPI.Controllers
     public class SwipeListController : ControllerBase
     {
         private readonly ILogger<SwipeListController> _logger;
+        private double MatchingScore = 0.0;
 
         public SwipeListController(ILogger<SwipeListController> logger)
         {
             _logger = logger;
+            this.MatchingScore = Double.Parse(Environment.GetEnvironmentVariable("PUPPY_LOVE_MATCHING_SCORE"));
         }
 
         [HttpGet("{email}", Name = "GetListSwipes")]
         public string GetListSwipes(string email)
         {
-            int id = -1;
+            int currDogID = -1;
             List<int> swipedIds = new List<int>();
+            Dictionary<int, bool> swiped = new Dictionary<int, bool>();
+            List<Dog> returnDogs = new List<Dog>();
             DBConnection DB = DBConnection.Instance();
 
             if (DB.IsConnect())
             {
+                // Get list of dogs that have been swiped on by an owner
                 string query = $"select d.dog_id from dogs d inner join owners o on o.owner_id = d.owner_id where o.email = \'{email}\';";
                 MySqlCommand cmd = new MySqlCommand(query, DB.Connection);
                 MySqlDataReader reader = cmd.ExecuteReader();
@@ -33,7 +40,7 @@ namespace PuppyLoveAPI.Controllers
                 {
                     try
                     {
-                        id = Int32.Parse(reader.GetString(0));
+                        currDogID = Int32.Parse(reader.GetString(0));
                     }
                     catch (Exception e)
                     {
@@ -53,9 +60,10 @@ namespace PuppyLoveAPI.Controllers
                         int id_1 = Int32.Parse(matchReader.GetString(0));
                         int id_2 = Int32.Parse(matchReader.GetString(1));
 
-                        if (id_1 == id && !swipedIds.Contains(id_2))
+                        if (id_1 == currDogID && !swipedIds.Contains(id_2))
                         {
                             swipedIds.Add(id_2); // only want the ones i have swiped on
+                            swiped.Add(id_2, true);
                         }
                     }
                     catch (Exception e)
@@ -73,16 +81,18 @@ namespace PuppyLoveAPI.Controllers
                 {
                     try
                     {
-                        int id_1 = Int32.Parse(matchReader.GetString(0));
-                        int id_2 = Int32.Parse(matchReader.GetString(1));
+                        int id_1 = Int32.Parse(swipeReader.GetString(0));
+                        int id_2 = Int32.Parse(swipeReader.GetString(1));
 
-                        if (id_1 == id && !swipedIds.Contains(id_2))
+                        if (id_1 == currDogID && !swipedIds.Contains(id_2))
                         {
                             swipedIds.Add(id_2);
+                            swiped.Add(id_2, true);
                         }
-                        else if (id_2 == id && !swipedIds.Contains(id_1))
+                        else if (id_2 == currDogID && !swipedIds.Contains(id_1))
                         {
                             swipedIds.Add(id_1);
+                            swiped.Add(id_1, true);
                         }
                     }
                     catch (Exception e)
@@ -93,10 +103,109 @@ namespace PuppyLoveAPI.Controllers
                 }
                 swipeReader.Close();
 
+
+                // return info for dogs that still need to be swiped on
+                string dogQuery = $"select * from dogs inner join owners o on o.owner_id = dogs.owner_id where dog_id != {currDogID};";
+                List<Tuple<Dog, Owner>> dogs = new List<Tuple<Dog, Owner>>();
+
+                MySqlCommand dogCmd = new MySqlCommand(dogQuery, DB.Connection);
+                MySqlDataReader dogReader = dogCmd.ExecuteReader();
+                while (dogReader.Read())
+                {
+                    int dogId = Int32.Parse(dogReader.GetString(0));
+                    int ownerId = Int32.Parse(dogReader.GetString(1));
+                    string dogName = dogReader.GetString(2);
+                    string breed = dogReader.GetString(3);
+                    int weight = Int32.Parse(dogReader.GetString(4));
+                    string age = dogReader.GetString(5);
+                    string sex = dogReader.GetString(6);
+                    int activityLevel = Int32.Parse(dogReader.GetString(7));
+                    bool vaccinationStatus = bool.Parse(dogReader.GetString(8));
+                    bool fixedStatus = bool.Parse(dogReader.GetString(9));
+                    string breedPreference = dogReader.GetString(10);
+                    string additionalInfo = dogReader.GetString(11);
+
+                    int owner_ownerId = Int32.Parse(dogReader.GetString(12));
+                    string ownerName = dogReader.GetString(13);
+                    string ownerEmail = dogReader.GetString(14);
+                    int owner_age = Int32.Parse(dogReader.GetString(15));
+                    int minAge = Int32.Parse(dogReader.GetString(16));
+                    int maxAge = Int32.Parse(dogReader.GetString(17));
+                    string owner_sex = dogReader.GetString(18);
+                    string sexPreference = dogReader.GetString(19);
+                    string location = dogReader.GetString(20);
+                    int maxDistance = Int32.Parse(dogReader.GetString(21));
+
+                    Owner owner = new Owner();
+                    owner.OwnerID = owner_ownerId;
+                    owner.OwnerName = ownerName;
+                    owner.OwnerEmail = ownerEmail;
+                    owner.Age = owner_age;
+                    owner.MinAge = minAge;
+                    owner.MaxAge = maxAge;
+                    owner.Sex = owner_sex;
+                    owner.SexPreference = sexPreference;
+                    owner.Location = location;
+                    owner.MaxDistance = maxDistance;
+
+                    Dog dog = new Dog();
+                    dog.DogID = dogId;
+                    dog.OwnerID = ownerId;
+                    dog.DogName = dogName;
+                    dog.Breed = breed;
+                    dog.Weight = weight;
+                    dog.Age = age;
+                    dog.Sex = sex;
+                    dog.ActivityLevel = activityLevel;
+                    dog.VaccinationStatus = vaccinationStatus;
+                    dog.FixedStatus = fixedStatus;
+                    dog.BreedPreference = breedPreference;
+                    dog.AdditionalInfo = additionalInfo;
+
+
+                    dogs.Add(new Tuple<Dog, Owner>(dog, owner));
+                }
+                dogReader.Close();
+
+                List<Tuple<Dog, Owner>> nonRepeatDogs = new List<Tuple<Dog, Owner>>();
+
+                // Get rid of dupes
+                for (int i = 0; i < dogs.Count; i++)
+                {
+                    if (!swiped.ContainsKey(dogs[i].Item1.DogID))
+                    {
+                        nonRepeatDogs.Add(dogs[i]);
+                    }
+                }
+
+                // Run matching algo
+                for (int i = 0; i < nonRepeatDogs.Count; i++)
+                {
+                    string html = string.Empty;
+                    string url = $"https://puppylovema.azurewebsites.net/api/puppylove?userEmail={email}&matchEmail={nonRepeatDogs[i].Item2.OwnerEmail}";
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader matchReader2 = new StreamReader(stream))
+                    {
+                        html = matchReader2.ReadToEnd();
+                    }
+
+                    double matchScore = Double.Parse(html);
+
+                    if (matchScore >= this.MatchingScore)
+                    {
+                        returnDogs.Add(nonRepeatDogs[i].Item1);
+                    }
+                }
+
                 DB.Close();
             }
 
-            return JsonSerializer.Serialize(swipedIds);
+            return JsonSerializer.Serialize(returnDogs);
         }
     }
 }
